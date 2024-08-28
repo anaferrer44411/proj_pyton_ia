67da228ccde56cbb66c10aae363207a27b032be2
Ana Gomes <v4nus01@gmail.com>
2024-08-28 18:55:36 -0300

    laço de repetição

---
+++ aula 03/para 2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "Ana",
+    "Malik",
+    "Kawan",
+    "Roberta",
+    "Isadora",
+    "Kauã",
+    "Diogo",
+    "Moisés",
+    "Luis",
+    "Gustavo"
+]
+alunos.sort()
+alunos.append("Roberto Carlos")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
