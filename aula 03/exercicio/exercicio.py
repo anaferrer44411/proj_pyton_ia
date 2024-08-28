27cde81935d1b70f3fa5fab215ddd4db9fd1809a
Ana Gomes <v4nus01@gmail.com>
2024-08-28 20:19:42 -0300

    exercicio.py

---
+++ aula 03/exercicio/exercicio.py
@@ -0,0 +1,19 @@
+Hortaliças = [
+    "Alface",
+    "Brócolis",
+    "Couve",
+    "Repolho",
+    "Milho-verde",
+    "Ervilha",
+    "Cenoura",
+    "Pepino",
+    "Pêssego",
+    "Uva"
+]
+Hortaliças.sort()
+Hortaliças.append("kiwi")
+Hortaliças.reverse()
+y = 1
+for x in Hortaliças:
+    print(y,"-",x)
+    y = y + 1
